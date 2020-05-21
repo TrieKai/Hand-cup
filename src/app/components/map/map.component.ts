abcf6bc2f166bf5d5611ad007018b4ac21abdc33
TrieKai <trie55432@gmail.com>
2020-05-21 17:59:02 +0800

    add getTopLocation func

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { GeolocationService } from 'src/app/service/geolocation.service';
 import { MapService } from 'src/app/service/map.service';
 import { ConstantsService } from 'src/app/util/constants/constants.service';
+import { DrinkShopService } from 'src/app/util/drinkShop/drink-shop.service';
 
 @Component({
     selector: 'app-map',
@@ -21,8 +22,8 @@ export class MapComponent implements OnInit {
     markers: google.maps.Marker[] = [];
     infoWindows: google.maps.InfoWindow[] = [];
     menus: Menu[] = [
-        { name: "今天飲料喝什麼!", icon: "folder", },
-        { name: "今天飲料喝哪家!", icon: "folder", },
+        { name: "今天飲料喝什麼?", icon: "folder", },
+        { name: "今天飲料喝哪家?", icon: "folder", },
         { name: "比較樣式", icon: "folder", },
         { name: "客製化推薦", icon: "folder", },
         { name: "我的收藏", icon: "folder", },
@@ -32,11 +33,12 @@ export class MapComponent implements OnInit {
         { name: "登出", icon: "apps" },
         { name: "設定", icon: "apps" },
         { name: "回報區", icon: "apps" },
-    ]
+    ];
 
     constructor(
         private geolocationService: GeolocationService,
         private mapService: MapService,
+        private drinkShopService: DrinkShopService,
         private cons: ConstantsService,
     ) { }
 
@@ -73,6 +75,7 @@ export class MapComponent implements OnInit {
         resp.body.map(resp => {
             this.resultArray.push(resp);
         });
+        this.resultArray = this.drinkShopService.getTopLocation(this.coordinate, this.resultArray, 5); // 抓附近的五個地點
         this.showAllLocation();
     }
 
--- src/app/util/drinkShop/drink-shop.service.ts
+++ src/app/util/drinkShop/drink-shop.service.ts
@@ -6,4 +6,18 @@ import { Injectable } from '@angular/core';
 export class DrinkShopService {
 
   constructor() { }
+
+  getTopLocation(locataion: Coordinate, dataList: any, number: number): any {
+    const resp = dataList.map((data) => {
+      const distanceGap = (locataion.latitude - data.longitude) + (locataion.longitude - data.latitute); // 距離差
+      return {
+        ...data,
+        distanceGap: distanceGap
+      }
+    }).sort((a, b) => {
+      return a - b; // 升冪
+    }).slice(0, number); // 取前幾名
+
+    return resp;
+  }
 }
