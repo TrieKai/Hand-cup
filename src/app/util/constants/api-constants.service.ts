df1f8c0008b5128e05f01a04681b71b239deb913
TrieKai <trie55432@gmail.com>
2020-05-31 20:55:51 +0800

    fix HTTP blocked by HTTPS

--- src/app/util/constants/api-constants.service.ts
+++ src/app/util/constants/api-constants.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 // const domain = 'localhost';
 // const port = ':5487';
 // const pre_url = domain + port;
-const pre_url = 'http://handcupbackend-env.eba-bfq2yt6j.ap-southeast-1.elasticbeanstalk.com';
+const pre_url = '//handcupbackend-env.eba-bfq2yt6j.ap-southeast-1.elasticbeanstalk.com';
 @Injectable({
   providedIn: 'root'
 })
