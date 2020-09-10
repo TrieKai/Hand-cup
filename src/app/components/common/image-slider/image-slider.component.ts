f8524061f95a79db64337f56e33c3c465795ef1c
TrieKai <trie55432@gmail.com>
2020-09-11 00:57:18 +0800

    Image slider change image

--- src/app/components/common/image-slider/image-slider.component.ts
+++ src/app/components/common/image-slider/image-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-image-slider',
@@ -6,19 +6,43 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./image-slider.component.scss']
 })
 export class ImageSliderComponent implements OnInit {
+  @ViewChild('preImage', { static: false }) preImage: ElementRef<HTMLDivElement>;
+  @ViewChild('nowImage', { static: false }) nowImage: ElementRef<HTMLDivElement>;
+  @ViewChild('subImage', { static: false }) subImage: ElementRef<HTMLDivElement>;
   @Input() width: number = 300;
   @Input() height: number = 300;
   @Input() images: string[];
+  showPreImg: boolean = false;
+  showNowImg: boolean = false;
+  showSubImg: boolean = false;
+  nowImageIndex: number = 0;
 
-  constructor() { }
+  constructor(
+    private renderer: Renderer2,
+  ) { }
 
   ngOnInit() {
+    this.showNowImg = true;
   }
 
-  containerStyles() {
+  ngAfterViewInit(): void {
+    this.renderer.setStyle(this.nowImage.nativeElement, 'background-image', 'url(' + this.images[this.nowImageIndex] + ')');
+  }
+
+  containerStyles(): object {
     return {
       'width': this.width + 'px',
       'height': this.height + 'px'
     };
+  }
+
+  changePreImage() {
+    this.nowImageIndex -= 1;
+    this.renderer.setStyle(this.nowImage.nativeElement, 'background-image', 'url(' + this.images[this.nowImageIndex] + ')');
+  }
+
+  changeSubImage() {
+    this.nowImageIndex += 1;
+    this.renderer.setStyle(this.nowImage.nativeElement, 'background-image', 'url(' + this.images[this.nowImageIndex] + ')');
   }
 }
