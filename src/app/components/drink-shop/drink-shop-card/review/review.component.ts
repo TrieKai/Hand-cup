0553aed19adc313c447d6cce033d13c7a12b662d
TrieKai <trie55432@gmail.com>
2020-11-13 12:56:30 +0800

    [fix] review error

--- src/app/components/drink-shop/drink-shop-card/review/review.component.ts
+++ src/app/components/drink-shop/drink-shop-card/review/review.component.ts
@@ -18,4 +18,8 @@ export class ReviewComponent implements OnInit {
   ngOnInit() {
     this.componentKey = this.cons.SHAREDDATA.reviewComponentRef;
   }
+
+  openUrl(url: string): void {
+    window.open(url, '_blank');
+  }
 }
